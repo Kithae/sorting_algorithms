ad27cde8e9d6ff128d8c1914f1858fe44e5bf314
Rachel <rachelkithae@yahoo.com>
2023-07-22 09:48:32 -0700

    test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,53 +1,54 @@
 #include "sort.h"
 
 /**
- * locate_min - function to find minimum array index
- * @array: an array
- * @index: first search Index
- * @size: array size
- * Return: (int) an index
-*/
+ * locate_min - Locatin the min From Current index in the array
+ * @array: The Array to be Searched
+ * @index: Starting Index of The Search
+ * @size: The Size of The Array
+ * Return: (int) index of min if found or
+ * same given index if index is the min
+ */
 
 int locate_min(int *array, int index, size_t size)
 {
-	int small, idx_small;
-	int a;
+	int min, idx_min;
+	int i;
 
-	small = array[index];
-	idx_small = index;
-	for (a = index; a < (int)size; a++)
+	min = array[index];
+	idx_min = index;
+	for (i = index; i < (int)size; i++)
 	{
-		if (array[a] < small)
+		if (array[i] < min)
 		{
-			small = array[a];
-			idx_small = a;
+			min = array[i];
+			idx_min = i;
 		}
 	}
-	if (idx_small == index)
+	if (idx_min == index)
 		return (-1);
-	return (idx_small);
+	return (idx_min);
 }
 
 /**
- * selection_sort - function to select sorted algo
- * @array: an array
- * @size: array size
+ * selection_sort - Implementation of selection Sort Algrithme
+ * @array: Array to sort type int *
+ * @size: The Size of The Given Array
  *
- * Return: the Sorted Array
+ * Return: (Void) Sorted Array
  */
 void selection_sort(int *array, size_t size)
 {
-	int a;
-	int small, tmp;
+	int i;
+	int min, tmp;
 
-	for (a = 0; a < (int)size; a++)
+	for (i = 0; i < (int)size; i++)
 	{
-		small = locate_min(array, a, size);
-		if (small != -1)
+		min = locate_min(array, i, size);
+		if (min != -1)
 		{
 			tmp = array[i];
-			array[a] = array[small];
-			array[small] = tmp;
+			array[i] = array[min];
+			array[min] = tmp;
 			print_array(array, size);
 		}
 	}
